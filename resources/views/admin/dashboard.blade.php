711c006e269dfb03c5a67a340a540c6b880ee1e7
Joydeep Dutta <joydeep.d@theaquarious.com>
2020-02-11 20:51:03 +0530

    Dyeing Knitting 11-02-2020

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -13,7 +13,6 @@
         <li class="active">Dashboard</li>
       </ol>
     </section>
-
     <!-- Main content -->
     <section class="content">
       <!-- Small boxes (Stat box) -->
@@ -23,7 +22,6 @@
           <div class="small-box bg-aqua">
             <div class="inner">
               <h3>150</h3>
-
               <p>New Orders</p>
             </div>
             <div class="icon">
@@ -38,7 +36,6 @@
           <div class="small-box bg-green">
             <div class="inner">
               <h3>53<sup style="font-size: 20px">%</sup></h3>
-
               <p>Bounce Rate</p>
             </div>
             <div class="icon">
@@ -53,7 +50,6 @@
           <div class="small-box bg-yellow">
             <div class="inner">
               <h3>44</h3>
-
               <p>User Registrations</p>
             </div>
             <div class="icon">
@@ -68,7 +64,6 @@
           <div class="small-box bg-red">
             <div class="inner">
               <h3>65</h3>
-
               <p>Unique Visitors</p>
             </div>
             <div class="icon">
@@ -77,18 +72,11 @@
             <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
           </div>
         </div>
-
       </div>
-
-      <div class="row">
-
+      <!-- <div class="row">
         <section class="col-lg-12 connectedSortable">
-
           <div class="nav-tabs-custom">
-
             <ul class="nav nav-tabs pull-right">
-              <!-- <li class="active"><a href="#revenue-chart" data-toggle="tab">Area</a></li>
-              <li><a href="#sales-chart" data-toggle="tab">Donut</a></li> -->
               <li class="pull-left header"><i class="fa fa-inbox"></i> Questions</li>
             </ul>
             <div class="tab-content no-padding">
@@ -96,14 +84,7 @@
               <div class="chart tab-pane" id="sales-chart" style="position: relative; height: 300px;"></div>
             </div>
           </div>
-
         </section>
-
-
-
-      </div>
-
-
-    </section>
-
+      </div> -->
+   </section>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -9,215 +9,168 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 /*Route::get('/', function () {
-    return view('welcome');
+return view('welcome');
 });*/
 
-Route::get('/', 
-		array('as' => 'home',
-			'uses' => 'HomeController@index'
-));
-
-
-Route::group(['middleware' => 'admin_guest'], function() {
-
-
-	Route::get('/admin', 
-		array('as' => 'admin-login',
-			'uses' => 'AdminAuth\LoginController@showLoginForm'
-	));
-	
-	Route::get('/admin/login', 
-		array('as' => 'admin-login',
-			'uses' => 'AdminAuth\LoginController@showLoginForm'
-	));
-
-
-
-	Route::post('/admin/login', 
-		array('as' => 'admin-login',
-			'uses' => 'AdminAuth\LoginController@login'
-	));	
-
-	Route::get('/admin/twof-login', 
-		array('as' => 'admin-twof-login',
-			'uses' => 'AdminAuth\LoginController@twof_login'
-	));	
-
-	Route::post('/admin/twof-login', 
-		array('as' => 'admin-twof-login',
-			'uses' => 'AdminAuth\LoginController@twof_login'
-	));	
-
-
-	Route::post('/admin/password-sent', 
-		array('as' => 'admin-password-sent',
-			'uses' => 'AdminAuth\LoginController@password_sent'
-	));
-
+Route::get('/',
+    array('as' => 'home',
+        'uses'     => 'HomeController@index',
+    ));
+Route::group(['middleware' => 'admin_guest'], function () {
+    Route::get('/admin',
+        array('as' => 'admin-login',
+            'uses'     => 'AdminAuth\LoginController@showLoginForm',
+        ));
+    Route::get('/admin/login',
+        array('as' => 'admin-login',
+            'uses'     => 'AdminAuth\LoginController@showLoginForm',
+        ));
+    Route::post('/admin/login',
+        array('as' => 'admin-login',
+            'uses'     => 'AdminAuth\LoginController@login',
+        ));
+    Route::get('/admin/twof-login',
+        array('as' => 'admin-twof-login',
+            'uses'     => 'AdminAuth\LoginController@twof_login',
+        ));
+    Route::post('/admin/twof-login',
+        array('as' => 'admin-twof-login',
+            'uses'     => 'AdminAuth\LoginController@twof_login',
+        ));
+    Route::post('/admin/password-sent',
+        array('as' => 'admin-password-sent',
+            'uses'     => 'AdminAuth\LoginController@password_sent',
+        ));
 });
-
 Route::prefix('admin')->group(function () {
-	
-	Route::group(array('namespace'=>'AdminAuth', 'middleware' => 'admin_auth'), function()
-	{
-		
-		Route::get('/logout', 
-		  array('as' => 'admin-logout',
-		  'uses' => 'LoginController@logout'
-		));
+    Route::group(array('namespace' => 'AdminAuth', 'middleware' => 'admin_auth'), function () {
+        Route::get('/logout',
+            array('as' => 'admin-logout',
+                'uses'     => 'LoginController@logout',
+            ));
+        Route::post('/logout',
+            array('as' => 'admin-logout',
+                'uses'     => 'LoginController@logout',
+            ));
+    });
+    Route::group(array('namespace' => 'Admin', 'middleware' => 'admin_auth'), function () {
 
-		Route::post('/logout', 
-		  array('as' => 'admin-logout',
-		  'uses' => 'LoginController@logout'
-		));
-		
-	
-	});
+        Route::get('/dashboard',
+            array('as' => 'admin-dashboard',
+                'uses'     => 'DashboardController@index',
+            ));
 
-	Route::group(array('namespace'=>'Admin', 'middleware' => 'admin_auth'), function()
-	{
+        Route::get('/change-password',
+            array('as' => 'admin-changepassword',
+                'uses'     => 'DashboardController@changepassword',
+            ));
 
-		Route::get('/dashboard', 
-		array('as' => 'admin-dashboard',
-		'uses' => 'DashboardController@index'
-		));	
+        Route::post('/change-password',
+            array('as' => 'admin-changepassword',
+                'uses'     => 'DashboardController@changepassword',
+            ));
 
+        Route::get('/change-profile',
+            array('as' => 'admin-changeprofile',
+                'uses'     => 'DashboardController@changeprofile',
+            ));
 
-        Route::get('/change-password', 
-		array('as' => 'admin-changepassword',
-		'uses' => 'DashboardController@changepassword'
-		));	
+        Route::post('/change-profile',
+            array('as' => 'admin-changeprofile',
+                'uses'     => 'DashboardController@changeprofile',
+            ));
 
-		Route::post('/change-password', 
-		array('as' => 'admin-changepassword',
-		'uses' => 'DashboardController@changepassword'
-		));	
+        Route::resource('users', 'UserController');
 
+        Route::post('users-index',
+            array('as' => 'users-index',
+                'uses'     => 'UserController@index',
+            ));
 
+        Route::post('users-delete',
+            array('as' => 'users-delete',
+                'uses'     => 'UserController@multi_destroy',
+            ));
 
-		Route::get('/change-profile', 
-		array('as' => 'admin-changeprofile',
-		'uses' => 'DashboardController@changeprofile'
-		));	
+        Route::get('/site-settings',
+            array('as' => 'site-settings',
+                'uses'     => 'SitesettingController@index',
+            ));
 
-		Route::post('/change-profile', 
-		array('as' => 'admin-changeprofile',
-		'uses' => 'DashboardController@changeprofile'
-		));	
-		
-		
-		Route::resource('users', 'UserController');	
+        Route::post('/site-settings',
+            array('as' => 'site-settings',
+                'uses'     => 'SitesettingController@index',
+            ));
 
-		Route::post('users-index', 
-		array('as' => 'users-index',
-			'uses' => 'UserController@index'
-        ));	
-	
-		Route::post('users-delete', 
-		array('as' => 'users-delete',
-			'uses' => 'UserController@multi_destroy'
-        ));
+        Route::post('questions-delete',
+            array('as' => 'questions-delete',
+                'uses'     => 'QuestionController@multi_destroy',
+            ));
 
-        Route::get('/site-settings', 
-		array('as' => 'site-settings',
-		'uses' => 'SitesettingController@index'
-		));	
+        Route::get('questions-activate/{id}',
+            array('as' => 'questions-activate',
+                'uses'     => 'QuestionController@question_activate',
+            ));
 
-		Route::post('/site-settings', 
-		array('as' => 'site-settings',
-		'uses' => 'SitesettingController@index'
-		));	
+        Route::get('questions-deactivate/{id}',
+            array('as' => 'questions-deactivate',
+                'uses'     => 'QuestionController@question_deactivate',
+            ));
 
-        Route::post('questions-delete', 
-		array('as' => 'questions-delete',
-			'uses' => 'QuestionController@multi_destroy'
-        ));
+        Route::get('approved-question',
+            array('as' => 'approved-question',
+                'uses'     => 'QuestionController@approved_question',
+            ));
 
-        Route::get('questions-activate/{id}', 
-		array('as' => 'questions-activate',
-			'uses' => 'QuestionController@question_activate'
-        ));
+        Route::get('rejected-question',
+            array('as' => 'rejected-question',
+                'uses'     => 'QuestionController@rejected_question',
+            ));
+        Route::resource('questions', 'QuestionController');
 
-
-        Route::get('questions-deactivate/{id}', 
-		array('as' => 'questions-deactivate',
-			'uses' => 'QuestionController@question_deactivate'
-        ));
-
-
-        Route::get('approved-question', 
-		array('as' => 'approved-question',
-			'uses' => 'QuestionController@approved_question'
-        ));
-
-        Route::get('rejected-question', 
-		array('as' => 'rejected-question',
-			'uses' => 'QuestionController@rejected_question'
-        ));
-
-        Route::resource('questions','QuestionController');
-
-	});
+    });
 });
+Route::group(['middleware' => 'user_guest'], function () {
+    Route::get('/user/register',
+        array('as' => 'user-register',
+            'uses'     => 'UserAuth\RegisterController@showRegistrationForm',
+        ));
+    Route::post('/user/register',
+        array('as' => 'user-register',
+            'uses'     => 'UserAuth\RegisterController@register',
+        ));
+    Route::get('/user/login',
+        array('as' => 'user-login',
+            'uses'     => 'UserAuth\LoginController@showLoginForm',
+        ));
 
-
-Route::group(['middleware' => 'user_guest'], function() {  
-
-     Route::get('/user/register', 
-		array('as' => 'user-register',
-			'uses' => 'UserAuth\RegisterController@showRegistrationForm'
-	));
-	Route::post('/user/register', 
-		array('as' => 'user-register',
-			'uses' => 'UserAuth\RegisterController@register'
-	));	
-	
-	
-	Route::get('/user/login', 
-		array('as' => 'user-login',
-			'uses' => 'UserAuth\LoginController@showLoginForm'
-	));
-
-	Route::post('/user/login', 
-		array('as' => 'user-login',
-			'uses' => 'UserAuth\LoginController@login'
-	));
+    Route::post('/user/login',
+        array('as' => 'user-login',
+            'uses'     => 'UserAuth\LoginController@login',
+        ));
 });
-
-
 Route::prefix('user')->group(function () {
-	
-	Route::group(array('namespace'=>'UserAuth', 'middleware' => 'user_auth'), function()
-	{
-		
-		Route::get('/logout', 
-		  array('as' => 'user-logout',
-		  'uses' => 'LoginController@logout'
-		));
-
-		Route::post('/logout', 
-		  array('as' => 'user-logout',
-		  'uses' => 'LoginController@logout'
-		));
-	
-	});
-
-	Route::group(array('namespace'=>'User', 'middleware' => 'user_auth'), function()
-	{		
-		Route::get('/dashboard', 
-		array('as' => 'user-dashboard',
-		'uses' => 'DashboardController@index'
-		));	
-
-		Route::post('/dashboard', 
-		array('as' => 'user-dashboard',
-		'uses' => 'DashboardController@index'
-		));
-		
-	});
+    Route::group(array('namespace' => 'UserAuth', 'middleware' => 'user_auth'), function () {
+        Route::get('/logout',
+            array('as' => 'user-logout',
+                'uses'     => 'LoginController@logout',
+            ));
+        Route::post('/logout',
+            array('as' => 'user-logout',
+                'uses'     => 'LoginController@logout',
+            ));
+    });
+    Route::group(array('namespace' => 'User', 'middleware' => 'user_auth'), function () {
+        Route::get('/dashboard',
+            array('as' => 'user-dashboard',
+                'uses'     => 'DashboardController@index',
+            ));
+        Route::post('/dashboard',
+            array('as' => 'user-dashboard',
+                'uses'     => 'DashboardController@index',
+            ));
+    });
 });
-
-
